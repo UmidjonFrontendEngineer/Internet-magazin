1317d5510315e71c2e35111929fdc6adfd772e49
Umidjon <umidjonsharipov283@gmail.com>
2026-07-05 17:25:00 +0500

    google search console, new git repo

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -32,15 +32,15 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" style={{ overflowX: 'hidden' }}>
-      <body>
-        <StylesProvider>
-            <HeaderC />
-            {children}
-            <CursorC />
-        </StylesProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en" style={{ overflowX: 'hidden' }}>
+            <body>
+                <StylesProvider>
+                    <HeaderC />
+                    {children}
+                    <CursorC />
+                </StylesProvider>
+            </body>
+        </html>
+    )
 }
