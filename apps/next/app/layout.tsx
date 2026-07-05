da3618ac61b8724c26e15f8d6d2f0734041b1e15
Umidjon <umidjonsharipov283@gmail.com>
2026-07-05 18:08:30 +0500

    fix: google search console verification meta tag

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -6,22 +6,24 @@ import CursorC from 'app/components/UI/Cursor'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-    title: "Uzum Market - O'zbekistondagi milliy marketplex",
+    title: "internet do'kon - O'zbekistondagi milliy marketplex",
     description: "Next.js texnologiyasida full-stack e-commerce App.",
-    keywords: ["nextjs", "react", "uzbekistan", "e-commerce", "internet do'kon", "uzum market"],
+    keywords: ["nextjs", "react", "uzbekistan", "e-commerce", "internet do'kon", "internet do'kon"],
+    verification: {
+        google: "vrW7yAymIBr4qV_SfSVrKjUcniOv6ICgCVgTwVE8GBQ",
+    },
 
     openGraph: {
-        title: "Uzum Market - O'zbekistondagi milliy marketplex",
+        title: "internet do'kon - O'zbekistondagi milliy marketplex",
         description: "Next.js texnologiyasida full-stack e-commerce App.",
-        url: "https://loyihangiz.uz",
-        siteName: "Uzum Market",
+        siteName: "internet do'kon",
         locale: "uz_UZ",
         type: "website",
     },
 
     twitter: {
         card: "summary_large_image",
-        title: "Uzum Market - O'zbekistondagi milliy marketplex",
+        title: "internet do'kon - O'zbekistondagi milliy marketplex",
         description: "Next.js texnologiyasida full-stack e-commerce App.",
     },
 
